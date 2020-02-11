62be8731e1de3158774afc1f477867f5652ea11b
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2020-02-11 22:26:33 +0300

    Дальше

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import App from "./components/app/app.jsx";
 
 const houses = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Wonderful hosue for rich people`];
 
+
 ReactDOM.render(<App houses={houses}/>, document.querySelector(`#root`));
