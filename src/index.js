50ab3950668507fba3340d37306e2de80bf1579b
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2020-02-17 21:19:46 +0300

    Небольшие правки

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import {offersData} from "./mocks/offers";
-
 ReactDOM.render(<App offersData={offersData}/>, document.querySelector(`#root`));
